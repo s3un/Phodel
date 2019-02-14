fe1f5230eeda49183ad550f922a97123603f6e7c
oluwaseun oyebowale <oluwaseunoyebowale@yahoo.com>
2019-02-14 04:14:53 -0800

    bootstrap

---
+++ java.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+    document.getElementById('circle').onclick=function () {
+        document.getElementById('circle').style.display="none;"
+        
+    }
+</script>
